6a2d5cff7e5c44b3dbda93220977fcdf702bea0c
datochichinadze <dchichinadze62@gmail.com>
2025-06-18 17:59:37 +0400

    ikam kna

---
+++ src/app/Search/Search.tsx
@@ -0,0 +1,13 @@
+import styles from './Search.module.scss';
+import Image from 'next/image';
+
+const Search = () => {
+
+    return (
+        <div>
+            
+            <input type="text" placeholder="Search" />
+        </div>
+    )
+}
+export default Search;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Search from "./Search/Search";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className={styles.container}>
+      <Search />
+    </div>
   );
 }
