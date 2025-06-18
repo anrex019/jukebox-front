538a249411f82542603af20b90f051662c00b984
datochichinadze <dchichinadze62@gmail.com>
2025-06-18 16:13:52 +0400

    feat: create reusable button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "./Button/Button";
 
 export default function Home() {
+
+  const useerLogdIn = true;
+
   return (
-    <div></div>
+    <div className={styles.container}>
+      {useerLogdIn 
+        ? <Button disabled={false} title="Neutral" />
+        : <Button disabled={true} title="Disabled" />
+      }
+    </div>
   );
 }
