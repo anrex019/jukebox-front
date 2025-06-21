6e238f2e6d25435eba8a00440a14196d7d4ed6e7
datochichinadze <dchichinadze62@gmail.com>
2025-06-21 16:25:51 +0400

    dasamtavrebelia

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,7 @@ import { AlbumCard } from './components/albumCard/AlbumCard';
 export default function HomePage() {
   return (
     <main className="main">
-      <AlbumCard
-        title="Eminem"
-        image="/images/eminem.jpg"
-        onClick={() => console.log('Clicked')}
-      />
+      
     </main>
   );
 }
