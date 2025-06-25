40075b062cdf67de93c5c35528199f3fb3c022d2
datochichinadze <dchichinadze62@gmail.com>
2025-06-25 18:04:52 +0400

    feat: Create header component

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -25,122 +25,115 @@ const Header = () => {
     };
 
     if (click) {
-      document.addEventListener('mousedown', clickOutside);
+      document.addEventListener("mousedown", clickOutside);
     }
 
     return () => {
-      document.removeEventListener('mousedown', clickOutside);
+      document.removeEventListener("mousedown", clickOutside);
     };
   }, [click]);
 
-
   return (
-    <div className={styles.container}>
-      <div className={styles.usersProfileStylesCOntainer}>
-        <Image
-          className={styles.imgStyles}
-          src=""
-          alt=""
-          width={52}
-          height={52}
-        />
-        <p className={styles.usertsTextContainer}>Music App</p>
+    <div className={styles.center}>
+      <div className={styles.container}>
+        <div className={styles.usersProfileStylesCOntainer}>
+          <Image
+            className={styles.imgStyles}
+            src=""
+            alt=""
+            width={52}
+            height={52}
+          />
+          <p className={styles.usertsTextContainer}>Music App</p>
+        </div>
+
+        <div className={styles.allImgContainerHeader}>
+          <div
+            onMouseEnter={() => setIsHoveredHome(true)}
+            onMouseLeave={() => setIsHoveredHome(false)}
+            className={styles.containerImgHeader}
+          >
+            <Image
+              className={styles.imgHeaderContainer}
+              src={isHoveredHome ? "/Home-Hover.svg" : "/Home.svg"}
+              alt="photo"
+              width={24}
+              height={24}
+            />
+            <p className={styles.headerTextContainer}>Home</p>
+          </div>
+
+          <div
+            className={styles.containerImgHeader}
+            onMouseEnter={() => setIsHoveredPlaylist(true)}
+            onMouseLeave={() => setIsHoveredPlaylist(false)}
+          >
+            <Image
+              className={styles.imgHeaderContainer}
+              src={isHoveredPlaylist ? "/Playlist-hover.svg" : "/Playlist.svg"}
+              alt="photo"
+              width={24}
+              height={24}
+            />
+            <p className={styles.headerTextContainer}>Playlist</p>
+          </div>
+
+          <div
+            className={styles.containerImgHeader}
+            onMouseEnter={() => setIsHoveredArtists(true)}
+            onMouseLeave={() => setIsHoveredArtists(false)}
+          >
+            <Image
+              className={styles.imgHeaderContainer}
+              src={isHoveredArtists ? "/Artists-hover.svg" : "/Artists.svg"}
+              alt="photo"
+              width={24}
+              height={24}
+            />
+            <p className={styles.headerTextContainer}>Artists</p>
+          </div>
+
+          <div
+            className={styles.containerImgHeader}
+            onMouseEnter={() => setIsHoveredCharts(true)}
+            onMouseLeave={() => setIsHoveredCharts(false)}
+          >
+            <Image
+              className={styles.imgHeaderContainer}
+              src={isHoveredCharts ? "/Charts-hover.svg" : "/Charts.svg"}
+              alt="photo"
+              width={24}
+              height={24}
+            />
+            <p className={styles.headerTextContainer}>Charts</p>
+          </div>
+
+          <div
+            className={styles.containerImgHeader}
+            onMouseEnter={() => setIsHoveredAlbums(true)}
+            onMouseLeave={() => setIsHoveredAlbums(false)}
+          >
+            <Image
+              className={styles.imgHeaderContainer}
+              src={isHoveredAlbums ? "/Albums-hover.svg" : "/Albums.svg"}
+              alt="photo"
+              width={24}
+              height={24}
+            />
+            <p className={styles.headerTextContainer}>Albums</p>
+          </div>
+        </div>
+        {click === false && (
+          <div onClick={onClicked} className={styles.searchContainer}>
+            <Image src="Search.svg" alt="Search" width={24} height={24} />
+          </div>
+        )}
+        {click && (
+          <div ref={searchRef}>
+            <Search />
+          </div>
+        )}
       </div>
-
-      <div className={styles.allImgContainerHeader}>
-        <div
-          onMouseEnter={() => setIsHoveredHome(true)}
-          onMouseLeave={() => setIsHoveredHome(false)}
-          className={styles.containerImgHeader}
-        >
-          <Image
-            className={styles.imgHeaderContainer}
-            src={isHoveredHome ? "/Home-Hover.svg" : "/Home.svg"}
-            alt="photo"
-            width={24}
-            height={24}
-          />
-          <p className={styles.headerTextContainer}>Home</p>
-        </div>
-
-        <div
-          className={styles.containerImgHeader}
-          onMouseEnter={() => setIsHoveredPlaylist(true)}
-          onMouseLeave={() => setIsHoveredPlaylist(false)}
-        >
-          <Image
-            className={styles.imgHeaderContainer}
-            src={isHoveredPlaylist ? "/Playlist-hover.svg" : "/Playlist.svg"}
-            alt="photo"
-            width={24}
-            height={24}
-          />
-          <p className={styles.headerTextContainer}>Playlist</p>
-        </div>
-
-        <div
-          className={styles.containerImgHeader}
-          onMouseEnter={() => setIsHoveredArtists(true)}
-          onMouseLeave={() => setIsHoveredArtists(false)}
-        >
-          <Image
-            className={styles.imgHeaderContainer}
-            src={isHoveredArtists ? "/Artists-hover.svg" : "/Artists.svg"}
-            alt="photo"
-            width={24}
-            height={24}
-          />
-          <p className={styles.headerTextContainer}>Artists</p>
-        </div>
-
-        <div
-          className={styles.containerImgHeader}
-          onMouseEnter={() => setIsHoveredCharts(true)}
-          onMouseLeave={() => setIsHoveredCharts(false)}
-        >
-          <Image
-            className={styles.imgHeaderContainer}
-            src={isHoveredCharts ? "/Charts-hover.svg" : "/Charts.svg"}
-            alt="photo"
-            width={24}
-            height={24}
-          />
-          <p className={styles.headerTextContainer}>Charts</p>
-        </div>
-
-        <div
-          className={styles.containerImgHeader}
-          onMouseEnter={() => setIsHoveredAlbums(true)}
-          onMouseLeave={() => setIsHoveredAlbums(false)}
-        >
-          <Image
-            className={styles.imgHeaderContainer}
-            src={isHoveredAlbums ? "/Albums-hover.svg" : "/Albums.svg"}
-            alt="photo"
-            width={24}
-            height={24}
-          />
-          <p className={styles.headerTextContainer}>Albums</p>
-        </div>
-      </div>
-      {click === false && (
-        <div onClick={onClicked} className={styles.searchContainer}>
-          <Image src="Search.svg" alt="Search" width={24} height={24} />
-        </div>
-      )}
-      {click && (
-        <div ref={searchRef}>
-          <Search />
-        </div>
-      )}
     </div>
   );
 };
-export default Header;
-function userRef(arg0: null) {
-  throw new Error("Function not implemented.");
-}
-function clickOutside(this: Document, ev: MouseEvent) {
-  throw new Error("Function not implemented.");
-}
-
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-'use client';
-import { AlbumCard } from './components/albumCard/AlbumCard';
-import Header from './components/Header/Header';
-import Search from './components/Search/Search';
-import styles from './page.module.css';
+"use client";
+import { AlbumCard } from "./components/albumCard/AlbumCard";
+import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
+import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
