e523a8936711393c905181592ac58958d8e2d652
anrex019 <anrex019@gmail.com>
2025-07-05 01:40:43 +0400

    feat:create popularArtist

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 
 "use client";
-import styles from "./page.module.css";
-import { AlbumCard } from "./components/albumCard/AlbumCard";
-import Player from "./components/Player/Player";
+import React from 'react';
+import PopularArtists from './components/PopularArtists/PopularArtists';
+
 
 export default function HomePage() {
   return (
-    <main className="main">
-      <div className={styles.container}>
-        <Player />
-      </div>
-    </main>
+    <div className="App">
+    <PopularArtists />
+  </div>
   );
 }
