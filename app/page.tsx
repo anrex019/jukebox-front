baf8521755d55ace58c3f25ad83cd6bdcf26e74d
Css1-1 <devnozag@gmail.com>
2025-07-04 05:55:31 +0400

    feat/created-reusable-table

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-
+import Table from "./components/table/table"
 import TopBanner from './components/topBanner/TopBanner';
+import image from "../public/images/DieWithSmile.png"
 
 export default function HomePage() {
   return (
     <main>
-   
+      <Table />
     </main>
   );
 }
