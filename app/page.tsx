be337b60adfb123536bea1bb07bdab8d7106fb86
anrex019 <anrex019@gmail.com>
2025-07-05 22:37:51 +0400

    fix conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,5 @@ import TopHits from './components/songListPage/TopHits';
 import TopCharts from './components/songListPage/TopCharts';
 
 export default function HomePage() {
-  return (
-    <main className="main">
 
-        </main>
-  );
-}
+};
