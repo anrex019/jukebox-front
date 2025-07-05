7e7e75d3f34380593592afa9f250bd9c38843a59
datochichinadze <dchichinadze62@gmail.com>
2025-07-05 17:26:27 +0400

    fix: pagetsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import styles from "./page.module.css";
-import { AlbumCard } from "./components/albumCard/AlbumCard";
-import Player from "./components/Player/Player";
 
 export default function HomePage() {
   return (
     <main className="main">
-      <div className={styles.container}>
-        <Player />
-      </div>
+
     </main>
   );
 }
