b64e9377524b96981b3cdbb4ce8abdc8888a389d
Css1-1 <devnozag@gmail.com>
2025-07-07 18:51:05 +0400

    fixed colors

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 import styles from "./page.module.css";
 import { AlbumCard } from "./components/albumCard/AlbumCard";
 import Player from "./components/Player/Player";
+import Button from "./components/Button/Button"
 
 export default function HomePage() {
   return (
     <main className="main">
       <div className={styles.container}>
-        <Player />
+        
       </div>
     </main>
   );
