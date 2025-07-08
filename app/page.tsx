a181df00912bba6fcb28c405fe698c45a7be5f34
datochichinadze <dchichinadze62@gmail.com>
2025-07-08 23:50:00 +0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
-    <main className="main">
+    <main className={styles.wrapper}>
     </main>
-
   );
 }
