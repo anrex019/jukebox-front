067d1c2a214e08e65648d433fd76bfc8a1377ed2
anrex019 <anrex019@gmail.com>
2025-07-09 14:30:03 +0400

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 
 "use client";
+<<<<<<< Updated upstream
 import TopHits from "./components/songListPage/TopHits";
+=======
+import TopCharts from "./components/songListPage/TopCharts";
+>>>>>>> Stashed changes
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
+<<<<<<< Updated upstream
     <main className="main">
       <TopHits/>
+=======
+    <main className={styles.wrapper}>
+      <TopCharts />
+>>>>>>> Stashed changes
     </main>
 
   );
