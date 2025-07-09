294494f1a8f6217bc5cd8d876aa5ff4271a92ebc
anrex019 <anrex019@gmail.com>
2025-07-09 14:42:33 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import TopCharts from "./components/songListPage/TopCharts";
 import TopHits from "./components/songListPage/TopHits";
@@ -6,10 +5,8 @@ import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
-    <main className="main">
-      <TopHits/>
-
+    <main className={"main"}>
+ 
     </main>
-
   );
 }
