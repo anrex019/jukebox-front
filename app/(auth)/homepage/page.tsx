f237ad4573868bb55d540e043cdaa576247e5b9c
datochichinadze <dchichinadze62@gmail.com>
2025-07-09 16:15:40 +0400

    comit need

--- app/(auth)/homepage/page.tsx
+++ app/(auth)/homepage/page.tsx
@@ -1,18 +1,26 @@
-import Header from '@/components/Header/Header';
-import styles from './page.module.scss';
-import TopBanner from '@/components/topBanner/TopBanner';
-import PopularArtists from '@/components/PopularArtists/PopularArtists';
+import Header from "@/components/Header/Header";
+import styles from "./page.module.scss";
+import TopBanner from "@/components/topBanner/TopBanner";
+import PopularArtists from "@/components/PopularArtists/PopularArtists";
+import MusicCard from "@/components/musicCard/musicCard";
+import TopHits from "@/components/songListPage/TopHits";
+import TopCharts from "@/components/songListPage/TopCharts";
 
 const HomePages = () => {
-
-    return(
-        <div className={styles.container}>
-            <div className={styles.headerContainer}>
-                <Header />
-                <TopBanner />
-            </div>
-            <PopularArtists />
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.headerContainer}>
+        <Header />
+        <TopBanner />
+      </div>
+      <div className={styles.hitStyle}>
+        <TopHits />
+      </div>
+      <PopularArtists />
+      <div className={styles.hitStyle}>
+        <TopCharts />
+      </div>
+    </div>
+  );
+};
 export default HomePages;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import HomePages from "./(auth)/homepage/page";
+import Player from "./components/Player/Player";
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
     <main className={styles.wrapper}>
       <HomePages />
-      
     </main>
   );
 }
