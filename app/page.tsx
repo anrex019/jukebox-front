b656c0f234ff28570d5db6dc683975f6f77001aa
anrex019 <anrex019@gmail.com>
2025-07-09 14:39:59 +0400

    fix top charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
 
 "use client";
-<<<<<<< Updated upstream
-import TopHits from "./components/songListPage/TopHits";
-=======
 import TopCharts from "./components/songListPage/TopCharts";
->>>>>>> Stashed changes
+import TopHits from "./components/songListPage/TopHits";
 import styles from "./page.module.css";
 
 export default function HomePage() {
   return (
-<<<<<<< Updated upstream
     <main className="main">
       <TopHits/>
-=======
-    <main className={styles.wrapper}>
-      <TopCharts />
->>>>>>> Stashed changes
+
     </main>
 
   );
