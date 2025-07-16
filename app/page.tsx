288af4d3c59267d4b4809fb842b88eea42e8bae5
anrex019 <anrex019@gmail.com>
2025-07-16 20:55:12 +0400

    feat:responsive-popular-artist

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+import PopularArtists from './(auth)/ArtistPage/ArtistPage';
 import HomePages from './(auth)/page';
 import styles from './page.module.css';
 
 export default function HomePage() {
   return (
     <main className={styles.wrapper}>
-      <HomePages />
+      <PopularArtists />
     </main>
   );
 }
