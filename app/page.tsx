926bf1ff385ba9c3de1289bfa5aa6f2e28288a29
datochichinadze <dchichinadze62@gmail.com>
2025-07-16 14:52:15 +0400

    refactor: layot.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import styles from './page.module.css';
-import StartLogIn from './StartPointLogIn/StartLogIn';
 
 export default function HomePage() {
   return (
     <main className={styles.wrapper}>
-      <StartLogIn />
     </main>
   );
 }
