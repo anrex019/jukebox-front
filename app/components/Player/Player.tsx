9c533078dc377278ae8b1450868fda4b2e5303c4
datochichinadze <dchichinadze62@gmail.com>
2025-07-24 01:20:57 +0400

    feat: popup player

--- app/components/Player/Player.tsx
+++ app/components/Player/Player.tsx
@@ -2,37 +2,7 @@
 import styles from "./Player.module.scss";
 import Image from "next/image";
 import { useAudioHook } from "../../Hooks/useAudioHook";
-
-const songs = [
-  {
-    id: 1,
-    artistName: "Star Boy",
-    songName: "The Weeknd",
-    image: "/StarBoy.png",
-    music: "/TheWeeknd.mp3",
-  },
-  {
-    id: 2,
-    artistName: "West Side",
-    songName: "Tupac",
-    image: "/TuPac.png",
-    music: "/2Pac.mp3",
-  },
-  {
-    id: 3,
-    artistName: "Steel D.R.E",
-    songName: "Snoop dog",
-    image: "/SnoopDog.png",
-    music: "/SnoopDog.mp3",
-  },
-  {
-    id: 4,
-    artistName: "Without me",
-    songName: "Eminem",
-    image: "/Eminem.png",
-    music: "/Eminem.mp3",
-  },
-];
+import { songs } from "./interface/songsProps.interface";
 
 const Player = () => {
   const {
